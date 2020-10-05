5d694ec3c2efa93f5c109e75f75cd6c879f0d2b1
11alexey11 <65157290+11alexey11@users.noreply.github.com>
2020-10-05 23:52:23 +0300

    feat: add toReadable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function toReadable (number) {
-  
+    let array = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 
+                'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let dozens = ['zero', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let arraySymbols = String(number).split('');
+    let str = '';
+    if (arraySymbols.length === 3) {
+        str += array[Number(arraySymbols[0])] + ' hundred';
+        if (Number(arraySymbols[1]) === 0) {
+            if (Number(arraySymbols[2]) !== 0) {
+                str += ' ' + array[Number(arraySymbols[2])];
+            }
+        }
+        else if (Number(arraySymbols[1]) === 1) {
+            str += ' ' + array[Number(arraySymbols[1] + arraySymbols[2])];
+        } else {
+            if (Number(arraySymbols[2]) !== 0) {
+                str += ' ' + dozens[Number(arraySymbols[1])] + ' ' + array[Number(arraySymbols[2])];
+            } else {
+                str += ' ' + dozens[Number(arraySymbols[1])];
+            }
+        }
+    } else if (arraySymbols.length === 2) {
+        if (Number(arraySymbols[0]) === 1) {
+            str += array[Number(arraySymbols[0] + arraySymbols[1])];
+        } else {
+            if (Number(arraySymbols[1]) !== 0) {
+                str += dozens[Number(arraySymbols[0])] + ' ' + array[Number(arraySymbols[1])];
+            } else {
+                str += dozens[Number(arraySymbols[0])];
+            }
+        }
+    } else {
+        str += array[Number(arraySymbols[0])];
+    }
+    return str;
 }
